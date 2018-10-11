f9817b1998e1d43213f6e7a64153185a1a7dfca3
Karla110297 <dinorakarla@hotmail.com>
2018-10-11 16:54:47 -0700

    Add project files.

---
+++ E2-2_Fibonacci_KarlaUrrea/OperacionesFibonacci.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2_2_Fibonacci_KarlaUrrea
+{
+    class OperacionesFibonacci
+    {
+        public OperacionesFibonacci() { }
+
+
+
+        public int Fibonacci(int n)
+        {
+            int nFibonacci = 1;
+            int x1=1,x2=1;
+
+            if (n>1)
+            {
+              
+                for (int i = 2; i <= n; i++)
+                {
+                    nFibonacci=x1+x2;
+                    x2= x1;
+                    x1 = nFibonacci;
+                    
+                }
+            }
+
+            return nFibonacci;
+        }
+
+        public int FibonacciRecursividad(int n)
+        {
+            if ((n == 0) || (n == 1))
+                return 1;
+            else
+                return FibonacciRecursividad(n - 1) + FibonacciRecursividad(n - 2);
+        }
+    }
+}
---
+++ E2-2_Fibonacci_KarlaUrrea/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2_2_Fibonacci_KarlaUrrea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperacionesFibonacci operacion = new OperacionesFibonacci();
+
+
+                Console.WriteLine("Numero en la sucesion fibonacci");
+                int n = Int32.Parse(Console.ReadLine());
+            
+
+            TimeSpan stop;
+            TimeSpan start = new TimeSpan(DateTime.Now.Ticks);
+
+
+            
+                Console.WriteLine("Fibbonacci de {0}= {1} ", n, operacion.FibonacciRecursividad(n));
+            
+
+            stop = new TimeSpan(DateTime.Now.Ticks);
+            Console.WriteLine("Tiempo: " + stop.Subtract(start).TotalMilliseconds);
+
+
+            Console.WriteLine();
+
+
+            TimeSpan parar;
+            TimeSpan comenzar = new TimeSpan(DateTime.Now.Ticks);
+
+            
+                Console.WriteLine("Fibonacci de {0}= {1} ", n, operacion.Fibonacci(n));
+           
+
+            parar = new TimeSpan(DateTime.Now.Ticks);
+            Console.WriteLine("Tiempo: " + parar.Subtract(comenzar).TotalMilliseconds);
+
+            Console.ReadKey();
+        }
+    }
+}
